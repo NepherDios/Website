2be3e041a3b13ce13a2c9522fb91fb706f2050d1
NepherDios <rui.moreira.programmer@gmail.com>
2026-06-29 17:31:28 +0100

    Added more images

--- imgs/test.py
+++ imgs/test.py
@@ -7,34 +7,29 @@ BASE = Path.cwd()
 
 # source file -> destination folder
 PRODUCTS = {
-    "caboclos":              "canecas/religiao/umbanda/000001-caboclo",
-    "orixa_ogum":            "canecas/religiao/umbanda/000002-ogum",
-    "orixa_oxala":           "canecas/religiao/umbanda/000003-oxala",
-    "pretos_velhos":         "canecas/religiao/umbanda/000004-pretos_velhos",
-    "ze_pelintra":           "canecas/religiao/umbanda/000005-ze_pelintra",
-    "caneca_de_oxumare":     "canecas/religiao/umbanda/000006-oxumare",
+    # -------------------------
+    # ANIVERSÁRIOS
+    # -------------------------
+    "feliz_aniversario":
+        "canecas/aniversarios/000001-feliz_aniversario",
 
-    "nossa_sr_fatima":       "canecas/religiao/catolicismo/000001-fatima",
-    "santo_antonio":         "canecas/religiao/catolicismo/000002-santo_antonio",
-    "sao_joao":              "canecas/religiao/catolicismo/000003-sao_joao",
-    "sao_pedro":             "canecas/religiao/catolicismo/000004-sao_pedro",
-    "senhor_dos_passos":     "canecas/religiao/catolicismo/000005-senhor_dos_passos",
+    "feliz_aniversario_v2":
+        "canecas/aniversarios/000002-feliz_aniversario_v2",
 
-    "clube_flamengo":        "canecas/desporto/futebol/000001-clube_flamengo",
-    "clube_porto":           "canecas/desporto/futebol/000002-clube_porto",
-    "portugal_selecao":      "canecas/desporto/futebol/000003-portugal_selecao",
+    # -------------------------
+    # DATAS COMEMORATIVAS
+    # -------------------------
+    "feliz_dia_das_maes":
+        "canecas/datas_comemorativas/000001-feliz_dia_das_maes",
 
-    "signo_peixes":          "canecas/signos/000001-peixes",
-    "signo_gemeos":          "canecas/signos/000002-gemeos",
-    "signo_balanca":         "canecas/signos/000003-balanca",
+    "feliz_dia_das_maes_v2":
+        "canecas/datas_comemorativas/000002-feliz_dia_das_maes_v2",
 
-    "ferrari":               "canecas/carros/000001-ferrari",
+    "feliz_dia_dos_pais":
+        "canecas/datas_comemorativas/000003-feliz_dia_dos_pais",
 
-    "caneca_red_dead_redemption":
-                              "canecas/jogos/000001-red_dead_redemption",
-
-    "keep_calm_self_believe":
-                              "canecas/frases/000001-keep_calm_self_believe",
+    "feliz_pascoa":
+        "canecas/datas_comemorativas/000004-feliz_pascoa",
 }
 
 
